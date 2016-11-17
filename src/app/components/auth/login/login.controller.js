f059bad8ebd426b58a91c27b9e22755f9c527503
Philip Rosen <philipjrosen@gmail.com>
2016-11-17 13:56:45 -0800

    add files for login and register components

---
+++ src/app/components/auth/login/login.controller.js
@@ -0,0 +1,7 @@
+function LoginController() {
+  let ctrl = this;
+}
+
+angular
+  .module('components.auth')
+  .controller('LoginController', LoginController);
---
+++ src/app/components/auth/register/register.controller.js
@@ -0,0 +1,7 @@
+function RegisterController() {
+  let ctrl = this;
+}
+
+angular
+  .module('components.auth')
+  .controller('RegisterController', RegisterController);
